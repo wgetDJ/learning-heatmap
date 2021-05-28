34d39f7318863543faa9edc3923e4023d252ce16
wgetdj <dibyajyotimishraiiit@gmail.com>
2021-05-29 00:31:57 +0530

    add todo HTML and CSS and currentDay JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 // JavaScript
+const currentDay = () => {
+    let today = new Date();
+    // let dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+    let monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Sep", "Oct", "Nov", "Dec"];
+    let day = today.getDate();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let currentMonth = monthList[month];
+    document.write(day+ " " + currentMonth + " " + year);
+}
